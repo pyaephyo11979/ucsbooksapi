74bae85627e7cf9d5c98d36e90e0c16b3f1f1f7b
Pyae Phyo Hlaing <www.pyaephyoehlaing16@gmail.com>
2023-11-15 11:21:16 +0630

    update

--- router.js
+++ router.js
@@ -3,11 +3,94 @@ const router = express.Router();
 const mongojs = require('mongojs');
 const bodyParser = require('body-parser');
 const { body, param, validationResult } = require('express-validator')
-const db = mongojs('mongodb+srv://admin:Pyare132605@ucsbooks.t0qcvni.mongodb.net/Books', ['books'],{
+const db = mongojs('mongodb+srv://admin:Pyare132605@ucsbooks.t0qcvni.mongodb.net/Books', ['books','users'],{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-router.get('/books/',(req,res)=>{
+const jwt=require('jsonwebtoken');
+const secret="universityofComputerStudies Sittwe Books Drake";
+router.post('/login',(req,res)=>{
+    const {usname,psw}=req.body;
+    db.users.find({username:usname,password:psw},(err,data)=>{
+        if(err){
+            res.sendStatus(500)
+        }else{
+            if(data.length){
+                const user=data[0];
+                const token=jwt.sign(user,secret);
+                return res.status(200).json({token})
+            }else{
+                return res.sendStatus(403)
+            }
+        }
+    })
+  
+})
+function auth(req,res,next){
+    const authHeader=req.headers['authorization'];
+    if(!authHeader) return res.sendStatus(401);
+    const [type,token]= authHeader.split(':');
+    if( type !== "Bearer") return res.sendStatus(401);
+    jwt.verify(token,secret,(err,data)=>{
+        if(err) return res.sendStatus(403);
+        else next();
+    })
+  }
+function onlyAdmin(req,res,next){
+    const [type,token]= req.headers['authorization'].split(':');
+    jwt.verify(token,secret,(err,user)=>{
+        if(user.role==="admin") next();
+        else res.sendStatus(403);
+    })
+}
+router.post('/register',(req,res)=>{
+    const {usname,psw}=req.body;
+    const rl="user";
+    db.users.find({username:usname},(err,data)=>{
+        if(err){
+            res.sendStatus(500)
+        }else{
+            if(data.length){
+                return res.sendStatus(403)
+            }else{
+                db.users.insert({username:usname,password:psw,role:rl},(err,data)=>{
+                    if(err){
+                        res.sendStatus(500)
+                    }else{
+                        const _id =data._id;
+                        res.append('Location',`/api/users/${_id}`);
+                        return res.status(201).json({meta:{_id},data})
+                    }
+                })
+            }
+        }
+    })
+   
+})
+router.post('/registerAdmin',(req,res)=>{
+    const {usname,psw}=req.body;
+    const rl = "admin";
+    db.users.find({username:usname},(err,data)=>{
+        if(err){
+            res.sendStatus(500)
+        }else{
+            if(data.length){
+                return res.sendStatus(403)
+            }else{
+                db.users.insert({username:usname,password:psw,role:rl},(err,data)=>{
+                    if(err){
+                        res.sendStatus(500)
+                    }else{
+                        const _id =data._id;
+                        res.append('Location',`/api/users/${_id}`);
+                        return res.status(201).json({meta:{_id},data})
+                    }
+                })
+            }
+        }
+    })
+})
+router.get('/books/',auth,(req,res)=>{
     const options=req.query;
     const sort=options.sort || {};
     const filter=options.filter || {} ;
@@ -29,7 +112,7 @@ router.get('/books/',(req,res)=>{
     })
 })
 //adding to DB
-router.post('/books',[
+router.post('/books',auth,onlyAdmin,[
     body("name").not().isEmpty(),
     body("year").not().isEmpty(),
     body("img").not().isEmpty(),
@@ -50,7 +133,7 @@ router.post('/books',[
     })
 })
 //put to DB
-router.put('/books/:id',[
+router.put('/books/:id',auth,onlyAdmin,[
     param('id').isMongoId(),
 ],(req,res)=>{
     const _id=req.params.id;
@@ -83,7 +166,7 @@ router.put('/books/:id',[
 })
 
 //Patch to DB
-router.patch('/books/:id',[
+router.patch('/books/:id',auth,onlyAdmin,[
     param('id').isMongoId(),
 ],(req,res)=>{
     const _id=req.params.id;
@@ -112,7 +195,7 @@ router.patch('/books/:id',[
 })
 
 //Remove data from db
-router.delete('/books/:id',[
+router.delete('/books/:id',auth,onlyAdmin,[
     param('id').isMongoId(),
 ],(req,res)=>{
     const _id=req.params.id;
